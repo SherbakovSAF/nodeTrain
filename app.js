688fe90d0c58b74236f125a140102d26fa669238
SherbakovSAF <sherbakovsaf@gmail.com>
2023-05-17 18:41:37 +0300

    Lesson 1

---
+++ app.js
@@ -0,0 +1,58 @@
+// const array = [{
+//      name: 'Sergey',
+//      surname: 'Sherbakov',
+//      age: 22,
+//      programmer: true,
+// },
+// {
+//      name: 'Vasya',
+//      surname: 'Pupkin',
+//      age: 98,
+//      programmer: false,
+// },
+// ]
+
+// array[1].name = 'Goga'
+
+// console.log(array[1])
+
+// Test end
+
+const fs = require('fs')
+const path = require('path')
+// Lesson 1
+// console.log(process)
+
+function textTest(){
+      fs.readFile('text.txt', 'utf-8', (error,data)=>{
+          console.log(data)
+     })
+     
+}
+
+function textTestSync(){
+     let text = fs.readFileSync('text.txt', 'utf-8')
+     console.log(text)
+}
+
+function readDirection(){
+     fs.readdir('textStorage',(error, data)=> {
+          data.forEach(e=> {
+               console.log(path.extname(e))
+               // text = fs.readFileSync(`./textStorage/${e}`, 'utf-8')
+               // console.log(text)
+          })
+     })
+}
+
+function readDirectionSync(){
+     let direction = fs.readdirSync('textStorage')
+     console.log(direction)
+     direction.forEach(e=>{
+          console.log(`Имя файла ${e}. Его размер ${fs.statSync(`textStorage/${e}`).size}. А его расширение ${path.extname(e)}`)
+     })
+     
+}
+
+readDirectionSync()
+
